4af55dab934ecc2daa02e6403f520eb4b9be6689
progspectar <progspectar@gmail.com>
2021-04-13 13:23:07 +0700

    basic app

--- push_an_existing_repo.bat
+++ push_an_existing_repo.bat
@@ -1,3 +1,4 @@
 rem git remote add origin https://github.com/progspectar/HandsOnTypeScript.git
 rem git branch -M main
-git push -u origin main
+rem git commit -m "basic app"
+rem git push -u origin main
