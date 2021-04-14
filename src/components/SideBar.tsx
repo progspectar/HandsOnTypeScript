4738a02f05950e86ae361c54549d9666f472a928
progspectar <progspectar@gmail.com>
2021-04-14 11:12:18 +0700

    sidebat

--- src/components/SideBar.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { IsMobileWidth } from "../hooks/useWindowDimensions";
-
-const SideBar = () => {
-  return IsMobileWidth() ? null : <div className="sidebar">Side Bar</div>;
-};
-
-export default SideBar;
